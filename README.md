1d87c88693a76aa02f097628dc962ea1854d60ed
stevegordon <tdtsgordon@gmail.com>
2020-03-10 23:47:47 -0700

    list class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KotlinPractice
 Work on Kotlin Koans/ practice for native Android App Development
+
+Compiler Command Reminder
+$ kotlinc [.kt_file] -include-runtime -d [.kt_filename].jar
--- practice.kt
+++ practice.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
     println("Hello, World!")
+    test()
 }
+
+var numbers = listOf("one", "two", "three", "four")
+fun test() {
+    println(numbers)
+}
+
